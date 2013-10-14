5a63e302ce551b6ae9c35c701ea6063e65503485
Cong Li <licong1112@gmail.com>
2013-10-14 17:05:22 +0800

    Added problem description

--- WordBreak.java
+++ WordBreak.java
@@ -1,3 +1,17 @@
+/**
+ * Practiced on 10/14/2013
+ * 
+ * Given a string s and a dictionary of words dict, determine if s 
+ * can be segmented into a space-separated sequence of one or more 
+ * dictionary words.
+ * 
+ * For example, given
+ * s = "leetcode",
+ * dict = ["leet", "code"].
+ * 
+ * Return true because "leetcode" can be segmented as "leet code".
+ */
+
 package com.congli.leetcode;
 
 import java.util.Set;
